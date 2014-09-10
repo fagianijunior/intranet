825b1e53d45b6b0766bbf469bc0c771d45f3c008
Carlos Fagiani Junior <fagianijunior@gmail.com>
2014-09-10 02:18:07 +0000

    tratamento usuario sem foto

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -21,8 +21,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  #  "images/fallback/" + [version_name, "default.png"].compact.join('_')
-    'default_avatar.png'
+  #  "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    [version_name, "avatar.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
@@ -64,3 +64,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
 end
+
